86c74b2b86bea55d2f849f820ae275a8055d9221
Andrei Bocan <zmaxor@gmail.com>
2014-10-19 13:07:20 +0300

    Actually legit working right now

--- server.rs
+++ server.rs
@@ -1,7 +1,7 @@
 use std::io::{TcpListener, TcpStream};
-use std::io::{Acceptor, Listener};
-use std::io::{IoError};
+use std::io::{Acceptor, Listener, IoError, IoResult};
 use std::io::util::copy;
+use std::io::net::addrinfo::get_host_addresses;
 use std::io::net::ip::{Ipv4Addr, SocketAddr};
 use std::time::duration::Duration;
 
@@ -28,37 +28,66 @@ fn handle_client(mut tcp_stream: TcpStream) -> Result<(), IoError> {
         let c = try!(tcp_stream.read_le_uint_n(1));
         let res = try!(tcp_stream.read_le_uint_n(1));
         let addr_type = try!(tcp_stream.read_le_uint_n(1));
+
+        let addr = try!(get_remote_addr(&mut tcp_stream, addr_type));
         
         println!("Process command {}", c);
         println!("res {}", res);
         println!("addr_type {}", addr_type);
 
-        let ip = try!(tcp_stream.read_exact(4));
-        let port = try!(tcp_stream.read_be_uint_n(2)).to_u16().unwrap();
-
-        let addr = SocketAddr{ ip: Ipv4Addr(ip[0], ip[1], ip[2], ip[3]), port: port };
-
-        println!("addr {}", addr);
         let mut outbound = try!(TcpStream::connect_timeout(addr, Duration::seconds(5)));
-        try!(tcp_stream.write(&[5, 0, 0, 1, 127, 0, 0, 1, 0, 0]));
+        println!("Connected {}", addr);
 
-        // println!(">> {}", try!(tcp_stream.read_exact(4)));
+        try!(tcp_stream.write(&[5, 0, 0, 1, 127, 0, 0, 1, 0, 0]));
 
         let mut client_reader = tcp_stream.clone();
         let mut socket_writer = outbound.clone();
+
+        println!("Started copy");
         spawn(proc() {
             let res_2 = copy(&mut client_reader, &mut socket_writer);
+            client_reader.close_read();
+            socket_writer.close_write();
         });
 
         let mut socket_reader = outbound.clone();
         let mut client_writer = tcp_stream.clone();
 
         let res_1 = copy(&mut socket_reader, &mut client_writer);
-
-
-        // p[0], ip[1], ip[2], ip[3]rocess_command(c, &mut tcp_stream)
+        socket_reader.close_read();
+        client_writer.close_write();
     }
+
     return Ok(())
+}
+
+fn get_remote_addr(tcp_stream: &mut TcpStream, addr_type: u64) -> IoResult<SocketAddr> {
+    match addr_type {
+        1 => {
+            let ip = try!(tcp_stream.read_exact(4));
+            let port = try!(tcp_stream.read_be_uint_n(2)).to_u16().unwrap();
+
+            return Ok(SocketAddr{ ip: Ipv4Addr(ip[0], ip[1], ip[2], ip[3]), port: port });
+        },
+        3 => {
+            let num_str = try!(tcp_stream.read_u8()).to_uint().unwrap();
+            println!("Reading {}", num_str);
+            let hostname_vec = try!(tcp_stream.read_exact(num_str));
+            let port = try!(tcp_stream.read_be_uint_n(2)).to_u16().unwrap();
+
+            let hostname = match String::from_utf8(hostname_vec) { Ok(s) => s, _ => "".to_string() };
+            let addresses = try!(get_host_addresses(hostname.as_slice()));
+
+            if addresses.is_empty() {
+                return Err(IoError::last_error())
+            } else {
+                println!("Resolution succeeded for {} - {}", hostname, addresses);
+                return Ok(SocketAddr{ ip: addresses[0], port: port });
+            }
+        },
+        _ => return Err(IoError::last_error())
+    }
+
 }
 
 fn process_command(command: u64, tcp_stream: &mut TcpStream) {
