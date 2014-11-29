b8a3244c829a35c9c550d97380019ae194706c7a
Andrei Bocan <zmaxor@gmail.com>
2014-11-29 11:06:25 +0200

    Handle authentication

--- src/main.rs
+++ src/main.rs
@@ -26,33 +26,63 @@ impl FromError<IoError> for RocksError {
     }
 }
 
+impl FromError<Vec<u8>> for RocksError {
+    fn from_error(err: Vec<u8>) -> RocksError {
+        RocksError::Generic("Invalid utf encountered".to_string())
+    }
+}
+
 impl FromError<String> for RocksError {
     fn from_error(string: String) -> RocksError {
         RocksError::Generic(string)
     }
 }
 
-fn handle_client(mut tcp_stream: TcpStream, logger: Logger, tracker: &ClientTracker) -> Result<(), RocksError> {
+fn authenticate(tcp_stream: &mut TcpStream) -> Result<(), RocksError> {
+    let version = try!(tcp_stream.read_u8());
+    if version != 1 {
+        return Err(FromError::from_error("Wrong version".to_string()))
+    }
+    let username_len = try!(tcp_stream.read_u8());
+    let username_bytes = try!(tcp_stream.read_exact(username_len as uint));
+    let username = try!(String::from_utf8(username_bytes));
+    let password_len = try!(tcp_stream.read_u8());
+    let password_bytes = try!(tcp_stream.read_exact(password_len as uint));
+    let password = try!(String::from_utf8(password_bytes));
+
+    // Success
+    tcp_stream.write(&[1, 0]);
+
+    println!("Authentication credentials {} {}", username, password);
+
+    Ok(())
+}
+
+fn handle_client(mut tcp_stream: TcpStream, logger: Logger) -> Result<(), RocksError> {
     loop {
-        let version = tcp_stream.read_le_uint_n(1);
-        match version {
-            Err(_) => break,
-            Ok(v) => if v == 5 {
-                let num_methods = try!(tcp_stream.read_le_uint_n(1));
-                println!("Process command num {}", num_methods);
-                let methods = try!(tcp_stream.read_be_uint_n(num_methods as uint));
-                println!("Process command poo {} {:X}", num_methods, methods);
-                tcp_stream.write(&[5, 0]);
+        let version = try!(tcp_stream.read_u8());
+        if version == 5 {
+            let num_methods = try!(tcp_stream.read_u8());
+            println!("Process command num {}", num_methods);
+            let methods = try!(tcp_stream.read_exact(num_methods as uint));
+            println!("Process command poo {} {}", num_methods, methods);
+            if methods.contains(&2) {
+                // Authenticated
+                tcp_stream.write(&[5, 2]);
+                try!(authenticate(&mut tcp_stream));
             } else {
-                drop(tcp_stream);
-                break
+                // Unauthenticated
+                tcp_stream.write(&[5, 0]);
             }
+        } else {
+            drop(tcp_stream);
+            break
         }
 
-        let v1 = try!(tcp_stream.read_le_uint_n(1));
-        let c = try!(tcp_stream.read_le_uint_n(1));
-        let res = try!(tcp_stream.read_le_uint_n(1));
-        let addr_type = try!(tcp_stream.read_le_uint_n(1));
+        let v1 = try!(tcp_stream.read_u8());
+        let c = try!(tcp_stream.read_u8());
+        let res = try!(tcp_stream.read_u8());
+        let addr_type = try!(tcp_stream.read_u8());
 
         let addr = try!(get_remote_addr(&mut tcp_stream, addr_type, &logger));
         
@@ -68,8 +98,6 @@ fn handle_client(mut tcp_stream: TcpStream, logger: Logger, tracker: &ClientTrac
         let mut client_reader = tcp_stream.clone();
         let mut socket_writer = outbound.clone();
 
-        tracker.increment();
-        println!("Started copy");
         spawn(proc() {
             let res_2 = copy(&mut client_reader, &mut socket_writer);
             client_reader.close_read();
@@ -170,9 +198,8 @@ fn main() {
                 println!("There was an error omg {}", e)
             }
             Ok(stream) => {
-                let tracker = trackers.get("Hello".to_string());
                 spawn(proc() {
-                    handle_client(stream, cloned_logger, &tracker);
+                    handle_client(stream, cloned_logger);
                 })
             }
         }
